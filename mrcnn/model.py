0c28a69bbb796180b97fcdf61a4c08d2847e3a60
IlyaShein <petrol.iva@yandex.ru>
2024-02-18 18:03:40 +0300

    Fix KE saving

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2142,9 +2142,10 @@ class MaskRCNN():
             layers = filter(lambda l: l.name not in exclude, layers)
 
         if by_name:
-            saving.load_weights_from_hdf5_group_by_name(f, layers)
+            hdf5_format.load_weights_from_hdf5_group_by_name(f, layers)
         else:
-            saving.load_weights_from_hdf5_group(f, layers)
+            hdf5_format.load_weights_from_hdf5_group(f, layers)
+
         if hasattr(f, 'close'):
             f.close()
 
